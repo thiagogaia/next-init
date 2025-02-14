7dedf7d36605c0237e7bfd158f1fa051cbf2f604
Thiago Gaia <thiagogaia@gmail.com>
2025-02-14 17:34:29 -0300

    COOMIT BOM INIT conceito server && client

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -58,16 +58,20 @@ const data = {
       isActive: true,
       items: [
         {
+          title: "Home",
+          url: "/dashboard",
+        },
+        {
           title: "History",
           url: "/dashboard/inbox",
         },
         {
-          title: "Posts",
+          title: "Posts [Server]",
           url: "/dashboard/posts",
         },
         {
-          title: "Settings",
-          url: "#",
+          title: "Posts [Client]",
+          url: "/dashboard/teste",
         },
       ],
     },
@@ -136,24 +140,7 @@ const data = {
         },
       ],
     },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
+  ]
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -164,7 +151,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
